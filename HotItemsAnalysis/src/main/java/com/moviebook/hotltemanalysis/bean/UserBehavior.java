084797b1abbcf0c7852230c1e244bb659c8340e2
MovieBook_xinll <17674033556@163.com>
2021-06-08 17:04:46 +0800

    first commit

---
+++ HotItemsAnalysis/src/main/java/com/moviebook/hotltemanalysis/bean/UserBehavior.java
@@ -0,0 +1,13 @@
+package com.moviebook.hotltemanalysis.bean;
+
+/**
+ * @ClassName: UserBehavior
+ * @Description: TODO
+ * @Author: MovieBook_xinll
+ * @Date: 2021/6/8 16:35
+ * @Version: v1.0
+ */
+public class UserBehavior {
+    // 定义私有属性
+
+}
